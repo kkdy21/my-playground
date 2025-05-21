77eb196086b46e6600ecf3c7722144cae88b5697
daeyeon ko <rowan@megazone.com>
2025-05-21 23:59:32 +0900

    fix: msw 초기화 순서 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
-import { useEffect } from "react";
 import "./App.css";
-import { initMSW } from "@/msw/bootstrap";
 import { useGetMenu } from "./repositories/menuRepository/query/useMenu";
 
 function App() {
-  useEffect(() => {
-    initMSW();
-  }, []);
   const { data } = useGetMenu();
   console.log(data);
   return <></>;
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { initMSW } from "./msw/bootstrap.ts";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -12,10 +13,16 @@ const queryClient = new QueryClient({
   },
 });
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
-  </StrictMode>
-);
+async function bootstrap() {
+  await initMSW();
+
+  createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </StrictMode>
+  );
+}
+
+bootstrap();
